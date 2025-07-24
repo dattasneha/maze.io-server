5195519d176c47003b0ac390604e8474ca054d99
Sneha Datta <snehadatta431@gmail.com>
2025-07-24 14:14:05 +0530

    Do minor fixes

--- routes/routes.config.js
+++ routes/routes.config.js
@@ -2,5 +2,5 @@ import express from "express";
 import authRouter from "./auth.routes.js"
 
 const routes = new express.Router();
-routes.route("/auth", authRouter);
+routes.use("/auth", authRouter);
 export default routes;
