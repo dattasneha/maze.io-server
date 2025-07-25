31adac1f9d9628dfc69fb5c769a8163414123ba9
Sneha Datta <snehadatta431@gmail.com>
2025-07-25 19:02:17 +0530

    Return game modes from db

--- controllers/gameModes.controller.js
+++ controllers/gameModes.controller.js
@@ -3,51 +3,14 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import ApiError from "../utils/apiError.js";
 import { STATUS } from "../constants/statusCodes.js";
 import { prisma } from "../utils/prismaClient.js";
-import { gameModeDataMap } from "../constants/gameModeMap.js";
 
 export const gameModes = asyncHandler(async (req, res) => {
-    const name = req.body.name;
-    if (!name) {
-        throw new ApiError(
-            STATUS.CLIENT_ERROR.NOT_ACCEPTABLE,
-            "Name is required!"
-        );
-    }
 
-    const gameModeData = gameModeDataMap[name];
-    if (!gameModeData) {
-        throw new ApiError(
-            STATUS.CLIENT_ERROR.NOT_ACCEPTABLE,
-            "Game mode name not found!"
-        );
-    }
-
-    const data = await prisma.gameMode.findUnique({
-        where: { id: gameModeData.id },
-        include: { options: true }
-    });
-
-    if (data) {
-        return res
-            .json(
-                new ApiResponse(
-                    data
-                )
-            );
-    }
-    const gameMode = await prisma.gameMode.create({
-        data: {
-            id: gameModeData.id,
-            name: name,
-            description: gameModeData.description,
-            options: {
-                create: gameModeData.options
-            }
-        },
+    const gameMode = await prisma.gameMode.findMany({
         include: {
             options: true
         }
-    });
+    })
 
     return res
         .status(STATUS.SUCCESS.OK)
